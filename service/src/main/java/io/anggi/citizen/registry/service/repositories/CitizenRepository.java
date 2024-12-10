33c87788e3f05e6788ff74bc9ef66ccb80484ecf
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-12-10 21:36:21 +0200

    feat: implement custom query methods for CitizenRepository

--- service/src/main/java/io/anggi/citizen/registry/service/repositories/CitizenRepository.java
+++ service/src/main/java/io/anggi/citizen/registry/service/repositories/CitizenRepository.java
@@ -6,6 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CitizenRepository extends JpaRepository<Citizen, String>, JpaSpecificationExecutor<Citizen> {
+    List<Citizen> findByIdNumber(String IdNumber);
+    List<Citizen> findByFirstName(String firstName);
+    List<Citizen> findByLastName(String lastName);
+    List<Citizen> findByFirstNameOrLastName(String firstName, String lastName);
+    List<Citizen> findByAfm(String afm);
+
 }
