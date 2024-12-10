6d64149b2c62bb7bf19f855f7b5f74268d9df6d1
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-12-10 21:49:21 +0200

    config: configure entity scanning for Citizen domain package

--- service/src/main/java/io/anggi/citizen/registry/service/ServiceApplication.java
+++ service/src/main/java/io/anggi/citizen/registry/service/ServiceApplication.java
@@ -2,8 +2,10 @@ package io.anggi.citizen.registry.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("io.anggi.citizen.registry.domain")
 public class ServiceApplication {
 
 	public static void main(String[] args) {
